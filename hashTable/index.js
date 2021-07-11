1f80dd2fa2a89fffd224fc9ede91ae148a2c93df
rpetzandrian <rpetzandrian@gmail.com>
2021-07-11 20:11:14 +0700

    adding data

--- hashTable/index.js
+++ hashTable/index.js
@@ -4,7 +4,11 @@ const ht = new hastTable()
 
 console.log(ht.set('Fahri'))
 console.log(ht.set('Fauzi'))
+console.log(ht.set('Gandhi'))
+console.log(ht.set('Budi'))
 
 console.log(ht.get('70-490'))
 console.log(ht.get('70-511'))
+console.log(ht.get('71-587'))
+console.log(ht.get('66-388'))
 
